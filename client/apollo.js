078b4e62613632acd473e3999d653a60d4b0b46a
David Christie <davidchristie.nz@gmail.com>
2017-04-28 14:30:58 +1200

    Refactor apollo client

--- client/apollo.js
+++ client/apollo.js
@@ -3,13 +3,14 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client'
 const networkInterface = createNetworkInterface({
   uri: 'https://us-west-2.api.scaphold.io/graphql/react-apollo-example'
 })
+
 networkInterface.use([{
-  applyMiddleware (req, next) {
-    if (!req.options.headers) {
-      req.options.headers = {}  // Create the header object if needed.
+  applyMiddleware (request, next) {
+    if (!request.options.headers) {
+      request.options.headers = {}
     }
     if (window.localStorage.getItem('token')) {
-      req.options.headers.Authorization = `Bearer ${window.localStorage.getItem('token')}`
+      request.options.headers.Authorization = `Bearer ${window.localStorage.getItem('token')}`
     }
     next()
   }
